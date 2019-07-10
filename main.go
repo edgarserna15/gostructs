506a0691a0eb1053c306c66c835e7bfd3a741e32
edgarserna15 <eserna03@gmail.com>
2019-07-09 19:49:29 -0500

    more structs

--- main.go
+++ main.go
@@ -2,12 +2,34 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	edgar := person{firstName: "Edgar", lastName: "Serna"}
-	fmt.Println(edgar)
+	edgar := person{
+		firstName: "Edgar",
+		lastName:  "Serna",
+		contactInfo: contactInfo{
+			email:   "eserna03@gmail.com",
+			zipCode: 72758,
+		},
+	}
+
+	edgar.updateName("ed")
+	edgar.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
